f8a207dac5fe624c6fbfa5ff51a42b0af14440f9
Charles Overbeck <charles.overbeck@gmail.com>
2020-05-24 22:30:41 -0700

    Explicitly specify content type

--- src/main/scala/org/overbeck/anagram/AnagramRoutes.scala
+++ src/main/scala/org/overbeck/anagram/AnagramRoutes.scala
@@ -7,7 +7,7 @@ case class AnagramRoutes()(implicit val log: cask.Logger) extends cask.Routes {
   @cask.postJson("/anagram")
   def findAnagrams(input: Value): Response[Value] = {
     try {
-      cask.Response(Anagrammer.matches(input.str))
+      cask.Response(Anagrammer.matches(input.str), 200, Seq(("Content-type", "application/json")))
     } catch {
       case ex: AnagrammerException => {
         log.exception(ex);

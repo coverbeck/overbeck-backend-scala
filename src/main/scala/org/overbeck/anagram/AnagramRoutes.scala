c9c7f02afeea25e576b210cb46332e6680513735
Charles Overbeck <charles.overbeck@gmail.com>
2020-11-29 19:52:02 -0800

    Use Scala Try/Success/Failure

--- src/main/scala/org/overbeck/anagram/AnagramRoutes.scala
+++ src/main/scala/org/overbeck/anagram/AnagramRoutes.scala
@@ -3,17 +3,19 @@ package org.overbeck.anagram
 import cask.model.Response
 import ujson.{Obj, Value}
 
+import scala.util.{Failure, Success}
+
 case class AnagramRoutes()(implicit val log: cask.Logger) extends cask.Routes {
   @cask.postJson("/anagram")
   def findAnagrams(input: Value): Response[Value] = {
-    try {
-      cask.Response(Anagrammer.matches(input.str), 200, Seq(("Content-type", "application/json")))
-    } catch {
-      case ex: AnagrammerException => {
-        log.exception(ex);
-        cask.Response(Obj("message" -> ex.getMessage), 400)
+      val matches = Anagrammer.matches(input.str)
+      matches match {
+        case Success(strs) => cask.Response(strs, 200, Seq(("Content-type", "application/json"))))
+        case Failure(ex) => {
+          log.exception(ex);
+          cask.Response(Obj("message" -> ex.getMessage), 400)
+        }
       }
-    }
   }
 
   initialize()

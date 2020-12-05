96fde06311d632fed57cd981afb874fcb3f0728d
Charles Overbeck <charles.overbeck@gmail.com>
2020-12-05 14:58:27 -0800

    Fix unit test failures

--- src/test/scala/org/overbeck/anagram/AnagrammerTest.scala
+++ src/test/scala/org/overbeck/anagram/AnagrammerTest.scala
@@ -4,10 +4,10 @@ import utest._
 
 object AnagrammerTest extends TestSuite {
   val tests = Tests {
-    val abcMatches = Anagrammer.matches("abc")
+    val abcMatches = Anagrammer.matches("abc").get
     assert(Seq("cab", "ab", "ba") == abcMatches)
 
-    val catalogMatchesLength = Anagrammer.matches("catalog").map(_.size)
+    val catalogMatchesLength = Anagrammer.matches("catalog").map(_.size).get
     assert(catalogMatchesLength == 60)
   }
 
